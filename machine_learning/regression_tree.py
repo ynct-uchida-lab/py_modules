7ab12f5bcad0f236507f31199662148bf4c81798
Harusak <sakaharu0211@gmail.com>
2020-06-18 11:20:04 +0900

    update file - regression_tree.py

--- machine_learning/regression_tree.py
+++ machine_learning/regression_tree.py
@@ -1,10 +1,13 @@
 #import
-import argparse
+import argparse #コマンドライン引数用
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets
 from sklearn import tree
+from sklearn.tree import plot_tree #可視化用
 
+
+#決定木分析 : Regression Tree クラス
 class RegressionTree():
     def __init__(self, max_depth):
         self.max_depth = max_depth
@@ -21,6 +24,21 @@ class RegressionTree():
     def training(self, train_data, train_target):
         self.clf.fit(train_data, train_target)
 
+    #可視化
+    def visualizetree(self, class_names, feature_names):
+        fig_tree = plt.figure(figsize = (16,8))
+        ax_tree = fig_tree.add_subplot(111)
+        plot_tree(self.clf, 
+                  class_names = class_names,
+                  feature_names = feature_names, 
+                  filled = True, 
+                  rounded = True, 
+                  proportion = True,
+                  fontsize = 10.5, 
+                  ax = ax_tree
+                  )
+        plt.show()
+        
 
 def main():
     #パラメータ
@@ -30,24 +48,29 @@ def main():
                         help = "tree depth (default = 3)",
                         type = int, 
                         default = 3)
+    parser.add_argument("-v", "--visualization", 
+                        help = "T/F output as png. (T:'-v', F:defo)", 
+                        action = "store_true")
     args = parser.parse_args()
 
     max_depth = args.max_depth #決定木の深さ
+    visualize = args.visualization #可視化の可否
 
     #irisデータセットを例に決定木分析する
     iris = datasets.load_iris()
-    
+
     data = iris.data[:, [0, 2]]
     target = iris.target
 
     #決定木分析
     clf = RegressionTree(max_depth) #-> __init__
     clf.training(data, target) #-> training
-
+    
+    #plot用にdataを分割
     x = data[:, 0]
     y = data[:, 1]
     
-    # meshを作成
+    #meshを作成
     x_min, x_max = x.min() - .5, x.max() + .5
     y_min, y_max = y.min() - .5, y.max() + .5
     x_mesh, y_mesh = np.meshgrid(np.arange(x_min, x_max, 0.05),
@@ -70,6 +93,12 @@ def main():
     plt.title(f'max_depth = {max_depth}')
 
     plt.show() 
+
+    #可視化
+    if (visualize):
+        target_names = iris.target_names
+        feature_names = iris.feature_names
+        clf.visualizetree(target_names, feature_names)
 
 if __name__ == '__main__':
     main()
