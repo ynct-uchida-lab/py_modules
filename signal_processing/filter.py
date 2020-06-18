4929a0b23a492931b169efc9590a2e5f5c9721c4
nishimura-16328 <64058801+nishimura-16328@users.noreply.github.com>
2020-06-18 12:02:19 +0900

    Create filter.py
    filter.pyのファイルを作成しました。

---
+++ signal_processing/filter.py
@@ -0,0 +1,38 @@
+﻿import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+#データにローパスフィルタ(バターワースフィルタ）をかける関数
+def lpf(x, samplerate, fp, fs, gpass, gstop):
+    fn = samplerate / 2                           #ナイキスト周波数
+    wp = fp / fn                                  #ナイキスト周波数で通過域端周波数を正規化
+    ws = fs / fn                                  #ナイキスト周波数で阻止域端周波数を正規化
+    N, Wn = signal.buttord(wp, ws, gpass, gstop)  #オーダーとバターワースの正規化周波数を計算
+    b, a = signal.butter(N, Wn, "band")           #フィルタ伝達関数の分子と分母を計算
+    data_lpf = signal.filtfilt(b, a, x)           #信号に対してフィルタをかける
+    return data_lpf                               #フィルタ後の信号を返す
+    
+def main():
+    samplerate = 25600                                   #波形のサンプリングレート
+    x = np.arange(0, 25600) / samplerate                 #波形生成のための時間軸の作成
+    data = np.random.normal(loc=0, scale=1, size=len(x)) #ガウシアンノイズを生成
+
+    dt=1/samplerate
+    t=np.arange(0,samplerate*dt,dt)
+ 
+    fp = np.array([1000,3000])     #通過域端周波数[Hz]
+    fs = np.array([500,600])       #阻止域端周波数[Hz]
+    gpass = 1                      #通過域端最大損失[dB]
+    gstop = 40                     #阻止域端最小損失[dB]
+    
+    
+    data_lpf = lpf(data, samplerate, fp, fs, gpass, gstop)
+    
+    plt.subplot(2,1,1)
+    plt.plot(t,data,label="original")
+    plt.plot(t,data_lpf,label="filter")
+    plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0, fontsize=8)
+    plt.show()
+    
+if  __name__  ==  "__main__":
+    main()
