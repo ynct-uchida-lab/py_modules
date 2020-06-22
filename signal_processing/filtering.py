e3869fc3b6bb0bef0944bf5bbdd0096b50192d19
nishimura-16328 <64058801+nishimura-16328@users.noreply.github.com>
2020-06-22 12:13:26 +0900

    Update filtering.py
    修正しました。

--- signal_processing/filtering.py
+++ signal_processing/filtering.py
@@ -1,9 +1,8 @@
 ﻿import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
-from fft import fft              #fft.pyのfft関数を呼び出す
 
-#データにbtypeに応じたフィルタをかける関数
+#データに指定されたフィルタをかける関数
 def butterworth(x, samplerate, fp, fs, gpass, gstop, btype):
     fn = samplerate / 2                           #ナイキスト周波数
     wp = fp / fn                                  #ナイキスト周波数で通過域端周波数を正規化
@@ -23,38 +22,27 @@ def main():
  
     fp = 3000                                     #通過域端周波数[Hz]
     fs = 6000                                     #阻止域端周波数[Hz]
-    gpass = 3                      #通過域端最大損失[dB]
-    gstop = 40                     #阻止域端最小損失[dB]
+    gpass = 3                                     #通過域端最大損失[dB]
+    gstop = 40                                    #阻止域端最小損失[dB]
     
     #データにローパスフィルタをかける
     data_lpf = butterworth(data, samplerate, fp, fs, gpass, gstop, 'low')
     #データにハイパスフィルタをかける
     data_hpf = butterworth(data, samplerate, fp, fs, gpass, gstop, 'high')
     
-    #データを高速フーリエ変換する
-    data_fft = fft(data)
-    #ローパスフィルタがかけられたデータを高速フーリエ変換する
-    data_lpf_fft = fft(data_lpf)
-    #ハイパスフィルタがかけられたデータを高速フーリエ変換する
-    data_hpf_fft = fft(data_hpf)
+    plt.subplots_adjust(hspace=0.6)
     
-    plt.subplots_adjust(hspace=1.0)
+    plt.subplot(2,2,1)
+    plt.plot(t,data)
+    plt.title("original",y=-0.4)
     
-    plt.subplot(3,1,1)
-    plt.plot(t,data,label="original")
-    plt.plot(t,data_lpf,label="filter_low")
-    plt.plot(t,data_hpf,label="filter_high")
-    plt.title("data",y=-0.7)
-    plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0, fontsize=8)
+    plt.subplot(2,2,3)
+    plt.plot(t,data_lpf,color='cyan')
+    plt.title("after lpf",y=-0.4)
     
-    plt.subplot(3,1,2)
-    plt.plot(data_fft,label="original")
-    plt.plot(data_lpf_fft,label="filter_low")
-    plt.plot(data_hpf_fft,label="filter_high")
-    plt.title("spectrum",y=-0.7)
-    plt.xlim(0,12800)
-    
-    plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0, fontsize=8)
+    plt.subplot(2,2,4)
+    plt.plot(t,data_hpf,color='red')
+    plt.title("after hpf",y=-0.4)
     
     plt.show()
     
