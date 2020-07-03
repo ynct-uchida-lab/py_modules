3147cf1ac535a7b0fe701cfb3d4bbdd09ad3b868
nishimura-16328 <64058801+nishimura-16328@users.noreply.github.com>
2020-07-03 11:26:44 +0900

    Update filtering.py
    fix_filtering

--- signal_processing/filtering.py
+++ signal_processing/filtering.py
@@ -14,12 +14,12 @@ def butterworth(x, samplerate, fp, fs, gpass, gstop, btype):
     return data_lpf                               # フィルタ後の信号を返す
 
 def main():
-    samplerate = 25600                                   # 波形のサンプリングレート
-    x = np.arange(0, 25600) / samplerate                 # 波形生成のための時間軸の作成
-    data = np.random.normal(loc=0, scale=1, size=len(x)) # ガウシアンノイズを生成
+    samplerate = 25600                                    # 波形のサンプリングレート
+    x = np.arange(0, 25600) / samplerate                  # 波形生成のための時間軸の作成
+    data = np.random.normal(loc=0, scale=1, size=len(x))  # ガウシアンノイズを生成
 
-    dt=1/samplerate                               # サンプリング間隔
-    t=np.arange(0,samplerate*dt,dt)               # 時間軸
+    dt = 1 / samplerate                               # サンプリング間隔
+    t = np.arange( 0, samplerate * dt, dt)               # 時間軸
 
     fp = 3000                                     # 通過域端周波数[Hz]
     fs = 6000                                     # 阻止域端周波数[Hz]
@@ -39,27 +39,27 @@ def main():
     
     plt.subplots_adjust(hspace=0.6)
     
-    plt.subplot(2,2,1)
-    plt.plot(t,data,label="original")
-    plt.plot(t,data_lpf,label="filter_low")
-    plt.title("original lpf",y=-0.4)
+    plt.subplot( 2, 2, 1)
+    plt.plot( t, data, label = "original")
+    plt.plot( t, data_lpf, label = "filter_low")
+    plt.title( "original lpf", y=-0.4)
     
-    plt.subplot(2,2,3)
-    plt.plot(data_fft,label="original")
-    plt.plot(data_lpf_fft,label="filter_low")
-    plt.title("spectrum lpf",y=-0.4)
+    plt.subplot( 2, 2, 3)
+    plt.plot( data_fft, label = "original")
+    plt.plot( data_lpf_fft, label = "filter_low")
+    plt.title( "spectrum lpf", y = -0.4)
     
-    plt.subplot(2,2,2)
-    plt.plot(t,data,label="original")
-    plt.plot(t,data_hpf,label="filter_high")
-    plt.title("original hpf",y=-0.4)
+    plt.subplot( 2, 2, 2)
+    plt.plot( t, data,label = "original")
+    plt.plot( t, data_hpf, label = "filter_high")
+    plt.title( "original hpf", y = -0.4)
     
-    plt.subplot(2,2,4)
-    plt.plot(data_fft,label="original")
-    plt.plot(data_hpf_fft,label="filter_high")
-    plt.title("spectrum hpf",y=-0.4)
+    plt.subplot( 2, 2, 4)
+    plt.plot( data_fft, label = "original")
+    plt.plot( data_hpf_fft, label = "filter_high")
+    plt.title( "spectrum hpf", y = -0.4)
     
     plt.show()
     
-if  __name__  ==  "__main__":
+if __name__ == "__main__":
     main()
