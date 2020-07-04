ff0335ff2584d252f017abb786408e9dc09699ba
uchikun2493 <um2493@gmail.com>
2020-07-04 11:13:17 +0900

    fix svm.py by pep8: 48, indent

--- machine_learning/svm.py
+++ machine_learning/svm.py
@@ -44,8 +44,7 @@ def main():
     # init setting
     # -------------------------------------
     # パラメータ
-    parser = argparse.ArgumentParser(
-            description="SVM using iris_dataset as an exam")
+    parser = argparse.ArgumentParser(description="SVM using iris_dataset as an exam")
     parser.add_argument("-c", "--c",
                         help="param C", 
                         type=float, 
