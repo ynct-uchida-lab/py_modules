03fb37a23860a2f6dd31b3511963956ed2e698c3
nishimura-16328 <64058801+nishimura-16328@users.noreply.github.com>
2020-07-22 11:44:03 +0900

    Update fft.py
    パワースペクトルを求める関数を追加しました

--- signal_processing/fft.py
+++ signal_processing/fft.py
@@ -1,5 +1,6 @@
 ﻿import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 
 # データを高速フーリエ変換しスペクトルを求める関数
 def fft(data):
@@ -8,10 +9,20 @@ def fft(data):
     spectrum = np.abs(data_fft)
     spectrum = spectrum[:int(len(spectrum) / 2)]
     return spectrum
+
+# データのパワースペクトルを求める関数
+def powerspectrum(data, fs, type):
+    freq, P =  signal.periodogram(data, fs)
     
+    # typeによってパワーを変更する
+    if (type == "dB"):
+        return freq, 10*np.log10(P)
+    else:
+        return freq, P
 def main():
     # サンプルデータの作成
-    dt = 0.01      # サンプリング周波数
+    dt = 0.01      # サンプリング周期
+    fs = 1 / dt    # サンプリング周波数
     tm = 1         # データの収録時間
     f1, f2 = 5, 8  # 周波数
     a1, a2 = 5, 4  # 信号の振幅
@@ -23,20 +34,37 @@ def main():
     
     # サンプルデータを高速フーリエ変換（FFT)
     spectrum = fft(data)
+    
+    # パワースペクトルを求める
+    freq_P, P = powerspectrum(data, fs, "none")
+    freq_P_dB, P_dB = powerspectrum(data, fs, "dB")
 
     # グラフをプロットする
-    plt.figure(2)
-    plt.subplot(211)
+    plt.subplots_adjust(wspace=0.4,hspace=1.0)
+
+    plt.subplot(321)
     plt.plot(t, data)
     plt.xlabel("time")
     plt.ylabel("amplitude")
+    plt.title("original")
 
-    plt.subplot(212)
+    plt.subplot(323)
     plt.plot(freq, spectrum)
     plt.xlabel("frequency")
     plt.ylabel("amplitude")
+    plt.title("spectrum")
+    
+    plt.subplot(325)
+    plt.plot(freq_P, P)
+    plt.xlabel("Frequency")
+    plt.ylabel("Power/frequency")
+    plt.title("power spectrum")
 
-    plt.tight_layout()
+    plt.subplot(326)
+    plt.plot(freq_P_dB, P_dB)
+    plt.xlabel("Frequency")
+    plt.ylabel("Power/frequency")
+    plt.title("power spectrum [dB]")
     
     plt.show()
 
