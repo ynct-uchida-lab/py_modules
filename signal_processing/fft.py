0be632325f3d5b5f129197d81a55d5efc14d68c8
uchikun2493 <um2493@gmail.com>
2020-08-27 09:32:31 +0900

    fix: calc

--- signal_processing/fft.py
+++ signal_processing/fft.py
@@ -3,22 +3,18 @@ import matplotlib.pyplot as plt
 from scipy import signal
 
 # データを高速フーリエ変換しスペクトルを求める関数
-def fft(data):
+def fft(data, dtype=None):
     data_fft = np.fft.fft(data)
     # 振幅を求めるために高速フーリエ変換されたデータを正規化
     spectrum = np.abs(data_fft)
     spectrum = spectrum[:int(len(spectrum) / 2)]
+
     return spectrum
 
-# データのパワースペクトルを求める関数
-def powerspectrum(data, fs, type):
-    freq, P = signal.periodogram(data, fs)
-    
-    # typeによってパワーを変更する
-    if (type == "dB"):
-        return freq, 10 * np.log10(P)
-    else:
-        return freq, P
+# 単位を'dB'に変換
+def amp_to_db(spectrum):
+    return 20 * np.log10(spectrum)
+
 def main():
     # サンプルデータの作成
     dt = 0.01      # サンプリング周期
@@ -27,45 +23,41 @@ def main():
     f1, f2 = 5, 8  # 周波数
     a1, a2 = 5, 4  # 信号の振幅
 
-    t = np.arange(0, tm, dt)                                                  # 時間
-    data = a1 * np.sin(2 * np.pi * f1 * t) + a2 * np.sin(2 * np.pi * f2 * t)  # サンプルデータ
-    freq = np.fft.fftfreq(len(data), dt)                                      # 周波数
+    # 時間
+    t = np.arange(0, tm, dt)
+    # サンプルデータ
+    data = a1 * np.sin(2 * np.pi * f1 * t) + a2 * np.sin(2 * np.pi * f2 * t)
+    # 周波数
+    freq = np.fft.fftfreq(len(data), dt)
     freq = freq[:int(len(freq) / 2)]
     
     # サンプルデータを高速フーリエ変換（FFT)
     spectrum = fft(data)
     
-    # パワースペクトルを求める
-    freq_P, P = powerspectrum(data, fs, "none")
-    freq_P_dB, P_dB = powerspectrum(data, fs, "dB")
+    # dB表記に直す
+    power = amp_to_db(spectrum)
 
     # グラフをプロットする
     plt.subplots_adjust(wspace=0.4, hspace=1.0)
 
-    plt.subplot(321)
+    plt.subplot(3, 1, 1)
     plt.plot(t, data)
     plt.xlabel("time")
     plt.ylabel("amplitude")
     plt.title("original")
 
-    plt.subplot(323)
+    plt.subplot(3, 1, 2)
     plt.plot(freq, spectrum)
     plt.xlabel("frequency")
     plt.ylabel("amplitude")
     plt.title("spectrum")
     
-    plt.subplot(325)
-    plt.plot(freq_P, P)
+    plt.subplot(3, 1, 3)
+    plt.plot(freq, power)
     plt.xlabel("Frequency")
-    plt.ylabel("Power/frequency")
+    plt.ylabel("dB")
     plt.title("power spectrum")
 
-    plt.subplot(326)
-    plt.plot(freq_P_dB, P_dB)
-    plt.xlabel("Frequency")
-    plt.ylabel("Power/frequency")
-    plt.title("power spectrum [dB]")
-    
     plt.show()
 
 if __name__ == "__main__":
